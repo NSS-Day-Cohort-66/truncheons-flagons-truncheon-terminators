5cb87dfe1b9f8d99ec7f77e5d6e99a9ca7ca5dd3
Nathan Chevalier <cerberusgfx@gmail.com>
2023-08-17 13:42:47 -0500

    Sort function in place for final output

--- scripts/Leaderboard.js
+++ scripts/Leaderboard.js
@@ -1,7 +1,17 @@
 const fetchScores = async () => {
-    const scores = await fetch("http://localhost:8088/scores?_expand=team").then((res) =>
-      res.json()
-    );
-    return scores;
-  };
-  
+  const scores = await fetch("http://localhost:8088/scores?_expand=team").then(
+    (res) => res.json()
+  );
+  return scores;
+};
+
+const leaderboard = await fetchScores();
+
+export const leaderboardOutput = () => {
+  const sorted = [...leaderboard].sort((a, b) => b.teamScore - a.teamScore);
+  let output = sorted.map((board) => {
+    return `${board.team.name} scored ${board.teamScore}`;
+  });
+  output.join("");
+  return output;
+};
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,12 @@
+import { leaderboardOutput } from "./Leaderboard.js";
+
 const mainContainer = document.querySelector(".container");
+
+const leaderboardHTML = leaderboardOutput();
 
 const renderInitialHTML = async () => {
   const outputHTML = `<h1>TEST YOUR CODE</h1>
+  ${leaderboardHTML}
     `;
 
   mainContainer.innerHTML = outputHTML;

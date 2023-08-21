6acc08480eb9fc877f3a648c5b692e55513e6078
Spencer Norris <Spencer.serv@gmail.com>
2023-08-21 09:50:08 -0500

    WIP

--- scripts/Rounds.js
+++ scripts/Rounds.js
@@ -1,6 +1,22 @@
+
+import { transientGameState } from "./currentGame.js"
+
+
 const teamSelect = ""
 
-const round1 = ""
+export const round1 = async () => {
+    const games = await transientGameState
+    const gameArr = games.map((game) => {
+        return `<section>
+        <h2 id="${game.teams.team1.teamId}">${game.teams.team1.name}</h2>
+        <h3>Current Score</h3><br>
+        ${game.teams.team1.score}
+        `
+    })
+
+
+
+}
 
 const round2 = ""
 
